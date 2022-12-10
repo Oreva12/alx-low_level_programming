70c138856f2af01935edd9e80cc6e8d9aa59c11d
Oreva12 <ovedheo@gmail.com>
2022-12-09 17:47:05 -0800

    Task 1 to 12

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * description: prints alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
