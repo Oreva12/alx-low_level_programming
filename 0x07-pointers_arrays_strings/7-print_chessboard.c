2eda17d1d0bdd9c57a6f34b0a003536e45926af8
Oreva12 <ovedheo@gmail.com>
2023-01-02 18:31:19 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - print_chessboard block
+ * @a: point type character
+ * Description: prints the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (col = 0; col < 8; col++)
+	{
+		for (row = 0; row < 8; row++)
+		{
+			_putchar(a[col][row]);
+		}
+		_putchar('\n');
+	}
+}
