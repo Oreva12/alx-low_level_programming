ad9b0a5105834955617389f0bb3793e16bc7679f
Oreva12 <ovedheo@gmail.com>
2023-05-24 06:06:48 -0700

    Make files

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,18 @@
+#include "m.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Priv
